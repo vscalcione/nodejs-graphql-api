990fc33df384563eed5978f71c8ad03f430fb29c
vscalcione <vincenzo99.scalcione@gmail.com>
2021-02-11 21:14:35 +0100

    Update project

---
+++ listings.js
@@ -0,0 +1,7 @@
+const listings = [
+    { id: "001", title: "Large ensuite condo", city: "Toronto" },
+    { id: "002", title: "Beverly Hills Mansion", city: "Los Angeles" },
+    { id: "003", title: "Small chic bedroom", city: "Dubai" }
+];
+
+exports.listings = listings;
--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 const { ApolloServer, gql } = require('apollo-server');
+const listings = require('./listings');
 
-const listings = [
+/*const listings = [
     { id: "001", title: "Large ensuite condo", city: "Toronto" },
     { id: "002", title: "Beverly Hills Mansion", city: "Los Angeles" },
     { id: "003", title: "Small chic bedroom", city: "Dubai" }
 ];
+*/
 
 const typeDefs = gql` type Listing { id: String! title: String! city: String! } type Query { listings: [Listing!]! } `;
 const resolvers = {
